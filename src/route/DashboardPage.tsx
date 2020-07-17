92d9f856ae75fa3d4e35e42691954f2557417b9f
Tyler Findlay <tyler@tyficonsulting.com>
2020-07-16 20:07:31 -0600

    text for dashboard

--- src/route/DashboardPage.tsx
+++ src/route/DashboardPage.tsx
@@ -5,10 +5,9 @@ import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
 import { connect } from 'react-redux';
 import { IAppState } from '../store';
+import { Typography } from '@material-ui/core';
 
 const styles = (theme: Theme) => createStyles({
-    root: {
-    },
 });
 
 interface IProps extends WithStyles<typeof styles> {
@@ -18,9 +17,38 @@ const DashboardPage: React.FC<IProps> = ({
     classes,
 }) => {
     return (
-        <div className={classes.root}>
-            Dashboard
-        </div>
+        <>
+            <Typography variant="h5">
+                Welcome to the first public test of Conference-Quest.
+            </Typography>
+            <Typography variant="subtitle1">
+                Thank you so much for being part of this test! 
+            </Typography>
+            <Typography variant="body1">
+                Be sure to come back and redeem your points for beer!
+            </Typography>
+            <Typography variant="body1">
+                We have a lot of great features on our road-map, but right now we are testing a couple of the functions with your help.
+            </Typography>
+            <Typography variant="body2">
+                You can create an account and profile.
+            </Typography>
+            <Typography variant="body2">
+                You can scan other peoples QR codes by using the scan button at the bottom of the screen.
+            </Typography>
+            <Typography variant="body2">
+                In the Top Left of the page you can access your QR code which will allow others to scan your phone screen and connect with you.
+            </Typography>
+            <Typography variant="body2">
+                Your scanned contacts are accessible via the Contacts Button and the calendar of events is available with the calendar button.
+            </Typography>
+            <Typography variant="body2">
+                Again, Thank you so much and enjoy the beer.
+            </Typography>
+            <Typography variant="h5">
+                - Tyler and Howard
+            </Typography>
+        </>
     );
 }
 
